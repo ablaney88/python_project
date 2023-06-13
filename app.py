80ee89c03aa7588c37f4537772130056e6d13425
ablaney88 <132158397+ablaney88@users.noreply.github.com>
2023-06-13 13:16:49 +0100

    another commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 
 from controllers.weapons_controller import weapons_blueprint
+from controllers.manufacturers_controller import manufacturers_blueprint
 
 
 app = Flask(__name__)
 
-app.register_blueprint(weapons_blueprint)
+app.register_blueprint(weapons_blueprint, manufacturers_blueprint)
 
 @app.route('/')
 def home():
