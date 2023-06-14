eb99692d7941631806c2fcc4d8f3d9bf59a4b57a
ablaney88 <132158397+ablaney88@users.noreply.github.com>
2023-06-15 00:17:28 +0100

    finished

--- console.py
+++ console.py
@@ -7,19 +7,35 @@ import repositories.weapon_repository as weapon_repository
 weapon_repository.delete_all()
 manufacturer_repository.delete_all()
 
-manufacturer1 = Manufacturer("Adeptus Mechanicus", "Producer of arms for the Imperium", "M25")
+manufacturer1 = Manufacturer("Adeptus Mechanicus", "Producer of arms for the Astartes", "M25")
 manufacturer_repository.save(manufacturer1)
 
-manufacturer2 = Manufacturer("Telchar", "Dwarven smith", "First Age")
+manufacturer2 = Manufacturer("Amethal: Forge World", "Producer of arms for the Imperium", "M25")
 manufacturer_repository.save(manufacturer2)
 
+manufacturer3 = Manufacturer("Deimos: Forge World", "Producer of arms for the Imperium", "M25")
+manufacturer_repository.save(manufacturer3)
 
+manufacturer4 = Manufacturer("Adeptus Mechanicus", "Producer of arms for the Astartes", "M25")
+manufacturer_repository.save(manufacturer4)
+
+manufacturer5 = Manufacturer("Adeptus Mechanicus", "Producer of arms for the Astartes", "M25")
+manufacturer_repository.save(manufacturer5)
 
 weapon1 = Weapon("Boltgun", manufacturer1, "Big gun kills xenos", 35, "Steel", 300, 500, 2)
 weapon_repository.save(weapon1)
 
-weapon2 = Weapon("Nasir", manufacturer2, "Sauron's worst nightmare", 2, "Steel", 1000, 3000, 8)
+weapon2 = Weapon("Lasgun", manufacturer2, "Pew Pew Pew", 2, "Steel", 100, 200, 300)
 weapon_repository.save(weapon2)
+
+weapon3 = Weapon("Chainsword", manufacturer3, "Chainsaw + Sword", 15, "Steel", 250, 400, 125)
+weapon_repository.save(weapon3)
+
+weapon4 = Weapon("Thunder Hammer", manufacturer4, "Hammer go smash", 150, "Steel", 1500, 4000, 10)
+weapon_repository.save(weapon4)
+
+weapon5 = Weapon("Plasma Pistol", manufacturer5, "Melt youre enemies", 6, "Steel", 800, 1400, 25)
+weapon_repository.save(weapon5)
 
 # weapon1.name ="abc"
 # manufacturer1.name = "abc"
